2bb0deacddf9eda7a9f3d427965dde98eda8c7de
Andrew Tachilin <AndrewTachilin@gmail.com>
2021-02-26 17:24:00 +0200

    redundant row

--- app/Requests/CurrencyExchange/CurrencyExchangeApiRequest.php
+++ app/Requests/CurrencyExchange/CurrencyExchangeApiRequest.php
@@ -14,7 +14,6 @@ class CurrencyExchangeApiRequest implements ExchangeApiInterface
 {
     private Client $client;
 
-
     public function __construct(Client $client)
     {
         $this->client = $client;
